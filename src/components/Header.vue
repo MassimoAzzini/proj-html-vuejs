5150116a01c57dcbc3419d29d523a3c7d14ffcb6
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-20 10:59:40 +0200

    [doc] scaffoldin assets

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -6,11 +6,55 @@ export default {
 </script>
 
 <template>
+  <header>
+    <div class="top-header">
+      <div class="container d-flex">
+        <div class="logo">
+          LOGO
+        </div>
+        <div class="nav">
+
+          <ul class="d-flex">
+            <li><a href="">Home</a></li>
+            <li><a href="">About Us</a></li>
+            <li><a href="">Vehicles</a></li>
+            <li><a href="">Dealers</a></li>
+            <li><a href="">Packages</a></li>
+            <li><a href="">Blog</a></li>
+            <li><a href="">Contact</a></li>
+            <li class="item-black"><a href="">icona My Account</a></li>
+            <li class="item-black"><a href="">icona Add Car</a></li>
+            <li><a href="">HM</a></li>
+            <li class="item-black"><a href="">icona number</a></li>
+          </ul>
+          
+        </div>
+      </div>
+    </div>
+    <div class="jumbotron">
+
+    </div>
+  </header>
   
 </template>
 
 
 
 <style lang="scss" scoped>
+.nav {
+
+  ul {
+    list-style: none;
+
+    li {
+      margin: 0 10px;
+
+      a {
+        text-decoration: none;
+
+      }
+    }
+  }
+}
 
 </style>
