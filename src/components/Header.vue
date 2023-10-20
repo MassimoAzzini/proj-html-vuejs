2652614141b42e49d434af49f62a92b71b7358e4
MassimoAzzini <massimo.azzini.web@gmail.com>
2023-10-20 12:13:37 +0200

    [add]: header menu

--- src/components/Header.vue
+++ src/components/Header.vue
@@ -7,14 +7,14 @@ export default {
 
 <template>
   <header>
-    <div class="top-header">
-      <div class="container d-flex">
+    <div class="top-header d-flex align-items-center">
+      <div class="container d-flex align-items-center">
         <div class="logo">
-          LOGO
+          <img class="w-100" src="/src/assets/logo/logo-autocar11.png" alt="">
         </div>
-        <div class="nav">
+        <div class="nav d-flex align-items-center">
 
-          <ul class="d-flex">
+          <ul class="d-flex align-items-center">
             <li><a href="">Home</a></li>
             <li><a href="">About Us</a></li>
             <li><a href="">Vehicles</a></li>
@@ -22,10 +22,25 @@ export default {
             <li><a href="">Packages</a></li>
             <li><a href="">Blog</a></li>
             <li><a href="">Contact</a></li>
-            <li class="item-black"><a href="">icona My Account</a></li>
-            <li class="item-black"><a href="">icona Add Car</a></li>
+            <li class="item-black">
+              <a href="">
+                <img src="/src/assets/icon/padlock.png" alt=""> 
+                My Account
+              </a>
+            </li>
+            <li class="item-black">
+              <a href="">
+                <img src="/src/assets/icon/add.png" alt="">
+                Add Car
+              </a>
+            </li>
             <li><a href="">HM</a></li>
-            <li class="item-black"><a href="">icona number</a></li>
+            <li class="item-black">
+              <a href="">
+                <img src="/src/assets/icon/headphones.png" alt="">
+                number
+              </a>
+            </li>
           </ul>
           
         </div>
@@ -41,20 +56,55 @@ export default {
 
 
 <style lang="scss" scoped>
-.nav {
+@use '../scss/partials/vars' as *;
 
-  ul {
-    list-style: none;
+header {
+  .top-header {
+    height: 75px;
 
-    li {
-      margin: 0 10px;
-
-      a {
-        text-decoration: none;
-
+  
+    .logo {
+      width: 200px;
+    }
+    .nav {
+    
+      ul {
+        list-style: none;
+        margin-bottom: 0;
+    
+        li {
+          margin: 0 2px;
+          padding: 12px 10px;
+          font-weight: 600;
+    
+          a {
+            text-decoration: none;
+            color: $first-color;
+            img {
+              width: 45px;
+              filter: invert(100%);              
+            }
+    
+          }
+        &.item-black {
+          background-color: $background-color;
+    
+          a {
+            color: $third-color;
+          }
+        }
+    
+        }
       }
     }
   }
+
+  .jumbotron {
+    height: 460px;
+    background-color: $background-color;
+  }
+
 }
+
 
 </style>
